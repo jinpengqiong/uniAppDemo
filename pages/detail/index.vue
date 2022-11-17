994a01d6b8e3000d09217e34741f2d4af86a36a8
Peng Jin <peng.jin@symbio.com>
2022-11-17 17:10:46 +0800

    first commit

---
+++ pages/detail/index.vue
@@ -0,0 +1,32 @@
+<template>
+	<view>
+		<view class="text-area">
+			<text class="title">{{title}}</text>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 666
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+	
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
+</style>
